340854daa6e8798e571961d727f1df1d9371f0c1
Beau Carnes <beaucarnes@gmail.com>
2019-01-01 09:44:03 -0500

    fix(donate): update stats as on 1 Jan 2019

--- src/components/Donation/components/DonateForm.js
+++ src/components/Donation/components/DonateForm.js
@@ -127,7 +127,7 @@ class DonateForm extends Component {
             people learn to code for free.
           </p>
           <p>
-            Join <strong>3,974</strong> supporters.
+            Join <strong>4,040</strong> supporters.
           </p>
           <p>
             Your $5 / month donation will help keep tech education free and
