d3f54b94e9ea97b862d76287b33e74b9dce926f9
Mrugesh Mohapatra <me@raisedadead.com>
2019-01-08 15:37:20 +0530

    fix: update donor stats - Jan 8, 2019

--- src/components/Donation/components/DonateForm.js
+++ src/components/Donation/components/DonateForm.js
@@ -127,7 +127,7 @@ class DonateForm extends Component {
             people learn to code for free.
           </p>
           <p>
-            Join <strong>4,040</strong> supporters.
+            Join <strong>4,116</strong> supporters.
           </p>
           <p>
             Your $5 / month donation will help keep tech education free and
